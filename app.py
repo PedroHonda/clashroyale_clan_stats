17aa2127a5d6ab4174f112675b281ff49a563937
Pedro Honda <pedro.koh@gmail.com>
2025-06-03 21:56:02 -0300

    Store the important queries in st.cache_data

--- app.py
+++ app.py
@@ -8,14 +8,25 @@ st.set_page_config(layout="wide")
 
 st.title("Clash Royale Clan Stats")
 
-clan_id = st.sidebar.selectbox("Enter clan ID:", ClashRoyaleClanRRLogDB.get_clans_from_db())
+@st.cache_data
+def get_clans_from_db():
+    return ClashRoyaleClanRRLogDB.get_clans_from_db()
 
-if clan_id:
+@st.cache_data
+def get_clan_riverracelog(clan_id):
     crs = ClashRoyaleClanRRLog(clan_id)
     season_data = crs.get_clan_riverracelog()
+    return season_data
+
+clans = get_clans_from_db()
+clan_id = st.sidebar.selectbox("Enter clan ID:", clans)
+
+if clan_id:
+    season_data = get_clan_riverracelog(clan_id)
 
     season_id = st.sidebar.selectbox("Select a season:", sorted(season_data.keys(), key=int, reverse=True))
     if st.sidebar.button("Update", type="primary"):
+        crs = ClashRoyaleClanRRLog(clan_id)
         crs.store_clan_riverracelog()
         st.sidebar.write("Updated!")
 
